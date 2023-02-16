95a95322e32da4d5b4e7ea2a101790d4f7c9686c
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-02-16 16:20:48 +0200

    fix: :bug: flight details card layout

--- dom.js
+++ dom.js
@@ -5,3 +5,14 @@ timeControl.addEventListener("input", () => {
   const date = new Date(timeControl.value);
   const seconds = Math.floor(date / 1000); //[Get the number of seconds since the ECMAScript Epoch](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#get_the_number_of_seconds_since_the_ecmascript_epoch)
 });
+
+//     Id = "3c6444", //0
+//     Callsign = "DLH9LF  ", //1
+//     OriginCountry = "Germany",//2
+//     Longitude = 6.1546,//5
+//     Latitude = 50.1964,//6
+//     IsOnGround = false,//8
+//     TrueTrackCompass = 98.26,//10
+//     Category = 1,//17
+
+
