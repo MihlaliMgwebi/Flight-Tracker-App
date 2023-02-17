22cd8ff155895acc7cae4ce576a324e7043f7c1b
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-02-17 06:32:18 +0200

    refactor: :fire: remove comments

--- dom.js
+++ dom.js
@@ -86,14 +86,3 @@ function createFlightCard(FlightDetails) {
   return card;
   // console.log(FlightDetails.ID);
 }
-// Click the time
-// Call API and load results
-
-//     Id = "3c6444", //0
-//     Callsign = "DLH9LF  ", //1
-//     OriginCountry = "Germany",//2
-//     Longitude = 6.1546,//5
-//     Latitude = 50.1964,//6
-//     IsOnGround = false,//8
-//     TrueTrackCompass = 98.26,//10
-//     Category = 1,//17
