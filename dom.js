ebb8c326f912f8b868e686a22db24167b0f5fc15
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-02-18 22:31:10 +0200

    feat: :sparkles: dynamically add details

--- dom.js
+++ dom.js
@@ -1,4 +1,5 @@
 // DOM Manipulation
+import { getFlightDetails, getSVGs } from "./mock";
 
 export function loadFlights() {
   //[Setting the value using JavaScript](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/time#setting_the_value_using_javascript)
@@ -7,16 +8,8 @@ export function loadFlights() {
   timeControl.addEventListener("input", () => {
     toggleFlightsVisibility();
     //TODO: Move to API, null coalescing
-    const flight = {
-      ID: 1, //null,
-      CALLSIGN: 1, //null,
-      ORIGIN_COUNTRY: 2, //null,
-      LONGITUDE: null,
-      LATITUDE: null,
-      IS_ON_GROUND: null,
-      TRUE_TRACK_COMPASS: null,
-    };
-    loadFlightDetails(flight);
+
+    loadFlightDetails(getFlightDetails());
   });
 }
 
@@ -27,21 +20,55 @@ function toggleFlightsVisibility() {
 }
 
 function loadFlightDetails(flight) {
-  const flightSummaryCollapsibleButtonId = `flight__summary--collapsible-${flight.ID}`;
-  const flightDetailsCardId = `flight__details-${flight.ID}`;
+  const flightSummaryCollapsibleButtonId = `flight__summary--collapsible-${flight.ICAO24}`;
+  const flightDetailsCardId = `flight__details-${flight.ICAO24}`;
 
   const flightSummaryCollapsibleButton = document.createElement("button");
   flightSummaryCollapsibleButton.id = flightSummaryCollapsibleButtonId;
-  flightSummaryCollapsibleButton.className = `flight__summary--collapsible-${flight.ID}`;
+  flightSummaryCollapsibleButton.className = `flight__summary--collapsible`;
   flightSummaryCollapsibleButton.innerHTML = `Flight ${flight.CALLSIGN} from orgin country ${flight.ORIGIN_COUNTRY}`;
 
   const flightDetailsCard = document.createElement("div");
   flightDetailsCard.id = flightDetailsCardId;
   flightDetailsCard.className = `flight__details`;
-  // console.log(document.getElementById(flightSummaryCollapsibleButtonId));
 
   const flightSummaryAndDetailsContainer =
     document.getElementById("app-main__flights");
   flightSummaryAndDetailsContainer.appendChild(flightSummaryCollapsibleButton);
   flightSummaryAndDetailsContainer.appendChild(flightDetailsCard);
+
+  createFlightDetailContainer(flightDetailsCardId);
 }
+
+function createFlightDetailContainer(flightDetailsCardId) {
+  Object.entries(getSVGs()).forEach((entry) => {
+    const [key, value] = entry;
+    console.log(value, key, flightDetailsCardId);
+  });
+  // container
+  // svg in container
+  // p in container
+  const flightDetailsCardProperty = document.createElement("div");
+  // flightDetailsCardProperty.id = `flight__property-${propertyKey}`;
+  // const flightDetailsCardPropertySVG = svg;
+  // const flightDetailsCardPropertyText = document.createElement("p");
+  // flightDetailsCardPropertyText.innerHTML = propertyValue;
+
+  const flightDetailsCard = document.getElementById(flightDetailsCardId);
+  flightDetailsCard.appendChild(flightDetailsCardProperty);
+}
+
+// TIME_POSITION: null,
+// LAST_CONTACT: null,
+// LONGITUDE: null,
+// BARO_ALTITUDE: null,
+// ON_GROUND: null,
+// VELOCITY: null,
+// TRUE_TRACK: null,
+// VERTICAL_RATE: null,
+// SENSORS: null,
+// GEO_ALTITUDE: null,
+// SQUAWK: null,
+// SPI: null,
+// POSITION_SOURCE: null,
+// CATEGORY: null,
