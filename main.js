4c5ed74d1e1d1ade06d2527b628a51575a10a275
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-02-23 07:35:31 +0200

    style: :art: remove dead code

--- main.js
+++ main.js
@@ -2,17 +2,3 @@ import { loadFlights, setMinTimeInput } from "./dom.js";
 setMinTimeInput();
 loadFlights();
 
-// import { getFirst20FlightDetails } from "./api.js";
-
-// // import { getFirst20FlightDetailsByTimeInMilliseconds } from "./api.js";
-// // // console.log(getFirst20FlightDetailsByTimeInMilliseconds());
-
-// getFirst20FlightDetails(1677149220).subscribe(
-//   (flightDetails) => {
-//     console.log(flightDetails);
-//   },
-//   (err) => {
-//     console.error(err);
-//   }
-// );
-// // console.log(getFirst20FlightDetails(1677149220));
