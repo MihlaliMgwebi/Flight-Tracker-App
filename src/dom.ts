80c9e9f2e67b9ee9101bd44600ff5a7d6f11c191
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-02-28 23:51:31 +0200

    feat: :construction: dom manipulation for buttons

--- src/dom.ts
+++ src/dom.ts
@@ -1,11 +1,16 @@
 // DOM Manipulation
-import {createLeafletMapWithMarkers$} from '../app/components/map/map'
+import { createFlightSummaryAndDetailsContainer$ } from '../app/components/flights/flight';
+import { createLeafletMapWithMarkers$ } from '../app/components/map/map';
 import { IFlights } from "./models/flight";
 
 export function manipulateDOM(flights: IFlights): void {
     const mapElement = document.getElementById('app-main__map')
     if (mapElement)
         createLeafletMapWithMarkers$.next(flights);
+
+    const flightSummaryAndDetailsContainer = document.getElementById('app-main__flights')
+    if (flightSummaryAndDetailsContainer instanceof HTMLDivElement)
+        createFlightSummaryAndDetailsContainer$.next(flights)
 }
 
 // //Chile not sure when to show and hide w/ time Input and button
