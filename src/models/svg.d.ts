e448ffe8ca6925fc6fa4e1e03c32fdc951890c06
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-03-03 07:53:59 +0200

    feat: :construction: add dom manipulation

---
+++ src/models/svg.d.ts
@@ -0,0 +1,27 @@
+interface IFlightSVGAndTextandUnits {
+    callsign: {
+        svg: string;
+        text: string;
+        units: string;
+    };
+    velocity: {
+        svg: string;
+        text: string;
+        units: string;
+    };
+    true_track: {
+        svg: string;
+        text: string;
+        units: string;
+    };
+    vertical_rate: {
+        svg: string;
+        text: string;
+        units: string;
+    }
+    geo_altitude: {
+        svg: string;
+        text: string;
+        units: string;
+    }
+}
