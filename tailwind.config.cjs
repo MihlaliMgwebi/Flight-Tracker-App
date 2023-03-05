dba563a0dbfa2fc107a0dc81978f63968cc3c9ac
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-03-05 12:48:19 +0200

    feat: :construction: styled main

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,18 @@ module.exports = {
   content: ["./index.html", "./src/**/*.ts"],
   theme: {
     extend: {
+      backgroundImage: {
+        cloudySky: "url('/assets/bg.avif')",
+      },
       colors: {
         custom: {
           blue: "rgb(77, 153, 231)",
           darkGrey: "#333",
           lightGrey: "#f1f1f1",
         },
+      },
+      boxShadow: {
+        whitesmoke: "0 0 1rem whitesmoke;",
       },
     },
   },
