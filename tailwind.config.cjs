893a4a54e3f36b00329fc93b417fe71380fc1256
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-03-05 10:28:44 +0200

    feat: :construction: add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,ts}"], // specify the files to scan so that Tailwind doesn't remove any classes you've defined but haven't used yet.
+  content: ["./index.html", "./src/**/*.ts"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          blue: "#ff49db",
+          grey: "#ff7849",
+        },
+      },
+    },
   },
   plugins: [],
 };
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./*.{html,ts}"], // specify the files to scan so that Tailwind doesn't remove any classes you've defined but haven't used yet.
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
