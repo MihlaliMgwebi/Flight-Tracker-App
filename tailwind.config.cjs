73ffe4ac32c13fc71212ae0de1edd911bc43d406
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-03-05 10:10:12 +0200

    feat: :heavy_plus_sign: add Automatic Class Sorting with Prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,ts}"], // specify the files to scan so that Tailwind doesn't remove any classes you've defined but haven't used yet.
   theme: {
     extend: {},
   },
