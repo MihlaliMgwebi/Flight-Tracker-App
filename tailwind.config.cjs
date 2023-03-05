fd21821d397a00d40abc8fb8dfdebc0c11f7d5e3
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-03-05 10:42:11 +0200

    feat: :construction: add grids

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,9 @@ module.exports = {
     extend: {
       colors: {
         custom: {
-          blue: "#ff49db",
-          grey: "#ff7849",
+          blue: "rgb(77, 153, 231)",
+          darkGrey: "#333",
+          lightGrey: "##f1f1f1",
         },
       },
     },
@@ -14,11 +15,3 @@ module.exports = {
   plugins: [],
 };
 
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./*.{html,ts}"], // specify the files to scan so that Tailwind doesn't remove any classes you've defined but haven't used yet.
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
